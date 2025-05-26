8ccca529f038b2480e5e87641cc55b134e8c5fcf
ashishmasi92 <ashishmasih92g@gmail.com>
2025-05-26 20:49:41 +0530

    create a assignment

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,24 +1,23 @@
-import React from "react";
-import "../styles/Child.css"
+import React, { useState } from "react";
+import "../styles/Child.css";
 
+const Selection = ({ applyColor }) => {
+  const [bgColor, setBgColor] = useState("");
 
+  const handleClick = () => {
+    if (applyColor) {
+      setBgColor(applyColor.backgroundColor);
+    }
+  };
 
-const Selection= ({key,applyColor})=>{
-function bgColor(e){
-    let target = e.target;
-target.setAttribute("data-testid",`${applyColor.backgroundColor}`)
-    target.style.backgroundColor=`${applyColor.backgroundColor}`
+  return (
+    <div
+      className="fix-box"
+      onClick={handleClick}
+      style={{ backgroundColor: bgColor }}
+      data-testid={bgColor}
+    ></div>
+  );
+};
 
-
-}
-    return(
-        <>
-
-        <div onClick={bgColor}  className="select_box">
-
-        </div>
-        </>
-    )
-}
-export default Selection
-
+export default Selection;
